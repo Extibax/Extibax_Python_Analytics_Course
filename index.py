d17e127601d10d782572c74d8553412428269a93
extibax <extibax@gmail.com>
2019-11-28 23:05:25 -0500

    templates folder created

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
-from flask import Flask
-import chalk
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == '__main__':
-    print(chalk.yellow("Server on port 5000"))
-    app.run(debug=True, port=3000)
+    print("Server on port 5000")
+    app.run(debug=True)
